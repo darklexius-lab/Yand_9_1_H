6fb37909acaa609c3aa157fb2da2382b3c822e31
drago <drago@DESKTOP-F40U465>
2026-04-06 16:26:43 +0300

    Добавьте файлы проекта.

---
+++ Yand_Test3_2/Program.cs
@@ -0,0 +1,86 @@
+﻿
+/*
+ string path = "input.txt";
+ string InTxt;
+
+System.IO.StreamReader reader = new System.IO.StreamReader(path);
+
+{
+    InTxt = reader.ReadLine();
+    
+}*/
+
+/*string InTxt = Console.ReadLine();
+InTxt = InTxt.Trim();
+int InInt[] = Array.ConvertAll(InTxt.Split(' '), int.Parse);*/
+
+string InTxt = Console.ReadLine();
+int buffer;
+InTxt = InTxt.Trim();
+buffer = Int32.Parse(InTxt);
+long A = buffer;
+
+InTxt = Console.ReadLine();
+InTxt = InTxt.Trim();
+buffer = Int32.Parse(InTxt);
+long B = buffer;
+
+InTxt = Console.ReadLine();
+InTxt = InTxt.Trim();
+buffer = Int32.Parse(InTxt);
+long C = buffer;
+
+long X=0;
+int caseCheck=0;
+
+if (A == 0)
+{
+    if (B == C * C)
+    {
+        caseCheck = 1;
+    }
+    else
+    {
+        caseCheck = 3; 
+    }
+}
+else 
+{
+    X = ((C * C) - B) / A;
+    if ((X * A + B) == (C * C))
+    {
+
+        caseCheck = 2;
+
+    }
+    else
+    {
+        caseCheck = 3;
+    }
+}
+
+if (C < 0) 
+{
+    caseCheck = 3;
+}
+
+switch (caseCheck)
+{
+    case 1:
+        InTxt = "MANY SOLUTIONS";
+        break;
+
+
+    case 2:
+        InTxt = "";    
+        InTxt +=X;
+        break;
+
+    case 3:
+        InTxt = "NO SOLUTION";
+        break;
+    default:
+        InTxt = "NO SOLUTION";
+        break;
+}
+Console.WriteLine(InTxt);
